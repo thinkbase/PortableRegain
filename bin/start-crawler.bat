9b76da782f41716fa2ec2a84c95f8d66e18b9038
trac-zh <trac-zh@lnspiron>
2014-02-06 02:14:40 +0800

    Add %JAVA_OPTS% to crawler, so some java options such as -Xmx can be adjusted by this variable.

--- bin/start-crawler.bat
+++ bin/start-crawler.bat
@@ -7,7 +7,7 @@ IF %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
 
 pushd "!REGAIN_HOME!/runtime/crawler"
 TITLE Regain crawler
-java -Xmx1024m -Dfile.encoding=UTF-8 -cp !IK_EXT_HOME!/bin;!IK_HOME!/IKAnalyzer2012_u6.jar;!REGAIN_HOME!/runtime/crawler/regain-crawler.jar net.sf.regain.crawler.Main -config "!PROFILE_HOME!/.work/CrawlerConfiguration.xml" -logConfig "!PROFILE_HOME!/.work/log4j.properties"
+java %JAVA_OPTS% -Dfile.encoding=UTF-8 -cp !IK_EXT_HOME!/bin;!IK_HOME!/IKAnalyzer2012_u6.jar;!REGAIN_HOME!/runtime/crawler/regain-crawler.jar net.sf.regain.crawler.Main -config "!PROFILE_HOME!/.work/CrawlerConfiguration.xml" -logConfig "!PROFILE_HOME!/.work/log4j.properties"
 popd
 
 ENDLOCAL
